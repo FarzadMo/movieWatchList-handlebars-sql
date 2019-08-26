9c35d1172746b702d9410fcc81bcdf0c4d8698f9
Fred M <farzad.moghbel@gmail.com>
2019-08-26 14:27:21 -0400

    the file is fully functional

--- server.js
+++ server.js
@@ -6,47 +6,52 @@ var app = express();
 
 var PORT = process.env.PORT || 8082;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
 var connection = mysql.createConnection({
     port: 3306,
     user: "root",
-    password: "farzad1365",
+    password: "",
     database: "movie_planner_db",
     host: "localhost"
 });
 
-connection.connect(function(err){
-    if(err) {
-        console.log("connection err"+ err.stack);
+connection.connect(function (err) {
+    if (err) {
+        console.log("connection err" + err.stack);
     }
-    console.log("connection connected as "+connection.threadId);
+    console.log("connection connected as " + connection.threadId);
 });
 
 //Routes
 
-app.get("/", function(req, res){
-    connection.query("SELECT * FROM movies;", function(err, result){
-        if(err){
+app.get("/", function (req, res) {
+    connection.query("SELECT * FROM movies;", function (err, result) {
+        if (err) {
             return res.status(500).end();
         }
 
-        res.render("index", {movies: result});
+        res.render("index", {
+            movies: result
+        });
     });
 });
 
 //DELETE
-app.delete("/api/movies/:id", function(req, res){
-    connection.query("DELETE FROM movies WHERE id = (?)", [req.params.id], function(err, result){
+app.delete("/api/movies/:id", function (req, res) {
+    connection.query("DELETE FROM movies WHERE id = (?)", [req.params.id], function (err, result) {
         //console.log("server delete result"+ result);
-        if(err){
+        if (err) {
             return res.status(500).end();
-        }
-        else if (result.affectedRows === 0){
+        } else if (result.affectedRows === 0) {
             return res.status(404).end();
         }
         res.status(200).end();
@@ -54,19 +59,40 @@ app.delete("/api/movies/:id", function(req, res){
 });
 
 //ADD NEW MOVIE
-app.post("/api/movies", function(req, res){
-    connection.query("INSERT INTO movies (movie) VALUES (?)", [req.body.movie], function(err, result){
-        if(err){
+app.post("/api/movies", function (req, res) {
+    connection.query("INSERT INTO movies (movie) VALUES (?)", [req.body.movie], function (err, result) {
+        if (err) {
             return res.status(500).end();
-        } 
-        res.json({ id: result.insertId });
+        }
+        res.json({
+            id: result.insertId
+        });
     });
 });
 
+//UPDATE MOVIE (PUT Method)
+app.put("/api/movies/:id", function (req, res) {
+    connection.query("UPDATE movies SET movie = ? WHERE id = ?", [req.body.movie, req.params.id], function (err, result) {
+        if (err) {
+            return res.status(500).end();
+        } else if (result.changedRows === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
+    });
+});
 
+//retrieving all data
+app.get("/api/movies", function (req, res) {
+    connection.query("SELECT * FROM movies;", function (err, data) {
+        if (err) {
+            return res.status(500).end();
+        }
 
+        res.json(data);
+    });
+});
 
-
-app.listen(PORT, function(){
-    console.log("Server is connected on https://localhost/"+PORT);
+app.listen(PORT, function () {
+    console.log("Server is connected on https://localhost/" + PORT);
 });
